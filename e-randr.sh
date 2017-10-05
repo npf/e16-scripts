ab51da6ffd774e157035d9abee4faae795f6e5d5
Pierre Neyron <pierre.neyron@imag.fr>
2017-10-05 09:47:17 +0200

    monoline xrandr -> breaks dual 4K :-(

--- e-randr.sh
+++ e-randr.sh
@@ -11,6 +11,7 @@ fi
 EXTDISPLAY=$(xrandr | grep " connected" | grep -v "$INTDISPLAY" | cut -f1 -d\ )
 
 get_eesh_windows_id() {
+  sleep 5
   PAGER_ID=$(eesh "wl" | grep "^0x[[:alnum:]]\+ : Pager-0$" | cut -d\  -f1)
   ICONBOX_ID=$(eesh "wl" | grep "^0x[[:alnum:]]\+ : Iconbox$" | cut -d\  -f1)
   GKRELLM_ID=$(eesh "wl" | grep "^0x[[:alnum:]]\+ : gkrellm$" | cut -d\  -f1)
@@ -44,12 +45,10 @@ xset -b
 
 if [ "$EXTRESOLUTION" == "3840x2160" ]; then
   nmcli radio wifi off
-  xrandr --output $INTDISPLAY --mode 3840x2160 --scale 0.666x0.666
-  xrandr --output $INTDISPLAY --pos 0x721
-  xrandr --output $EXTDISPLAY --mode 3840x2160 --scale 1x1
-  xrandr --output $EXTDISPLAY --pos 2558x0
+  xrandr --output $INTDISPLAY --mode 3840x2160 --scale 1x1
+  xrandr --output $INTDISPLAY --mode 3840x2160 --scale 0.666x0.666 --pos 0x721 --output $EXTDISPLAY --mode 3840x2160 --scale 1x1 --pos 2558x0
+	sleep 5
   eesh "restart" 
-  sleep 2
   get_eesh_windows_id
   wop $GKRELLM_ID move 2386 718
   wop $ICONBOX_ID move 2388 1877
@@ -67,14 +66,13 @@ if [ "$EXTRESOLUTION" == "3840x2160" ]; then
 else
 	nmcli radio wifi on
   xrandr --output $INTDISPLAY --mode 3840x2160 --scale 1x1
-  xrandr --output $INTDISPLAY --mode 3840x2160 --scale 0.666x0.666
-  xrandr --output $INTDISPLAY --pos 0x0
   if [ -n "$EXTRESOLUTION" ]; then
-    xrandr --output $EXTDISPLAY --mode $EXTRESOLUTION --scale 1x1
-    xrandr --output $EXTDISPLAY --pos 2558x0
+    xrandr --output $INTDISPLAY --mode 3840x2160 --scale 0.666x0.666 --pos 0x0 --output $EXTDISPLAY --mode $EXTRESOLUTION --scale 1x1 --pos 2558x0
+  else
+    xrandr --output $INTDISPLAY --mode 3840x2160 --scale 0.666x0.666 --pos 0x0
   fi
+	sleep 5
 	eesh "restart" 
-	sleep 2
   get_eesh_windows_id
   wop $GKRELLM_ID move 2386 0
   wop $ICONBOX_ID move 2388 1177
